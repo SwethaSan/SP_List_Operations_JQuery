3bcbb03c43b93120a6742f5a77f2c257047fdad3
Swetha Sankaran <swetha.sankaran@gmail.com>
2024-03-11 07:04:00 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,77 @@
+<style type="text/css">
+    .effect1 {
+        -webkit-box-shadow: 0 10px 6px -6px #777;
+        -moz-box-shadow: 0 10px 6px -6px #777;
+        box-shadow: 0 10px 6px -6px #777;
+        padding: 10px;
+    }
+</style>
+<div class="container-fluid effect1" style=" background-color: white; padding:20px">
+    <div class="row">
+        <div class="col-md-12">
+           <table class="table table-sm table-dark">
+			  <thead>
+			    <tr>
+			      <th scope="col">Business Domains</th>
+			      <th scope="col">Primary Contact</th>
+			      <th scope="col">Secondary Contact</th>
+			      <th scope="col">Functional Area</th>
+			    </tr>
+			  </thead>
+			  <tbody id='tblbody'>
+			    
+			   </tbody>
+			</table>
+        </div>
+    </div>
+</div>
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 <script type="text/javascript">
-$(document).ready(function() {
+var tblbody; 
+var bd;
+var pc;
+var sc;
+var fa;
+$(window).on('load', function() {
     SP.SOD.executeFunc('sp.js', 'SP.ClientContext', sharePointReady);
-
+    
     function sharePointReady() {
-        getItemsFromView("PageDescriptions", 'Risk Liaison', updateDescription, logError);
-    }
-
-    function updateDescription(items) {
-        var desc = '';
+    	var clientContext = new SP.ClientContext.get_current();
+    	var pageField = clientContext.get_web().get_lists().getByTitle('Pages').getItemById(_spPageContextInfo.pageItemId);
+    	clientContext.load(pageField);
+    	
+    	clientContext.executeQueryAsync(
+    		function(){
+    			var viewTitle = pageField.get_item('ArticleByLine');
+ 			   	if(viewTitle === '') {
+ 			   		console.error('unable to retrieve page property');
+ 			   		return
+ 			   	} else{
+        			getItemsFromView("KeyContacts", viewTitle, updateKeyContacts, logError);
+    			}
+    		},
+    		function(sender,args){
+    			console.error('Error: ' + args.get_message());
+    		}
+    	);	
+	}
+	
+    function updateKeyContacts(items) {
         for (var i = 0; i < items.get_count(); i++) {
             var item = items.get_item(i);
-            desc = item.get_item('Description');  // Assuming last item's description is needed
-        }
-        $("#descDiv").html(desc);
+            bd = item.get_item('Title'); 
+            pc = item.get_item('PrimaryContact');
+            sc = item.get_item('SecondaryContact');
+            fa = item.get_item('Notes');        
+         }
+        tblbody += '<tr>' +
+			       '<th scope="row" id="'+bd+'"></th>' +
+			       '<td id="'+fa+'"></td>' +
+			       '<td id="'+pc+'"></td>' +
+			       '<td id="'+sc+'"></td>' +
+			       '</tr>';        			
+        $("#tblbody").appendChild(tblbody);
     }
 
     function logError(sender, args) {
